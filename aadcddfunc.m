781fb9e017f660e2b3a9050a177cda410e92b149
daxjuanxiong <daxjuanxiong@users.noreply.github.com>
2022-11-10 17:44:24 +0800

    Add files via upload

--- aadcddfunc.m
+++ aadcddfunc.m
@@ -1,63 +1,59 @@
 % Small infrared target detection using absolute average difference weighted by cumulative directional derivatives
-% 1 双框计算对比度（置零）
-% 2 多尺度
+% 1 多尺度： 目标框的尺度3x3 - 9x9，背景框的尺度19x19，计算对比度 （置零）
+% 2 计算4个方向目标区域与背景区域的梯度 （置零）
+% 3 对比度与梯度相乘
 function re = aadcddfunc(img)
 img = double(img);
-%% the scale is 3
-out3 = subfubc(img, 3);
-%% the scale is 5
-out5 = subfubc(img, 5);
-%% the scale is 7
-out7 = subfubc(img, 7);
-%%  the scale is 9
-out9 = subfubc(img, 9);
+%% scale 3
+out3 = subfunc(img, 3);
+%% scale 5
+out5 = subfunc(img, 5);
+%% scale 7
+out7 = subfunc(img, 7);
+%%  scale 9
+out9 = subfunc(img, 9);
 %%
 out = cat(3, out3, out5, out7, out9);
 re = max(out, [], 3);
 if 0
     figure; imshow(out3, []);  title('scale 3x3');
-    figure; surf(out3); title('scale 3x3');
     figure; imshow(out5, []);   title('scale 5x5');
-    figure; surf(out5); title('scale 5x5');
     figure; imshow(out7, []);   title('scale 7x7');
-    figure; surf(out7); title('scale 7x7');
     figure; imshow(out9, []);   title('scale 9x9');
-    figure; surf(out9); title('scale 9x9');
+    figure; imshow(uint8(img));
     figure; imshow(re, []);
-    figure; surf(re);
 end
 end
-function re = subfubc(img, len)
-lx = floor(len/2);
-ly = floor(len/2);
-opin = ones(len)/(len*len);
-ain = imfilter(double(img), opin, 'symmetric');
-opout = ones(19 , 19);
-opout(10-ly:10+ly, 10-lx:10+lx) = 0;
-opout = opout/sum(opout(:));
-aout = imfilter(double(img), opout, 'symmetric');
-D1 = (ain - aout ).^2.*(ain - aout >=0);
+function re = subfunc(img, len)
+top = ones(len)/(len*len);
+mt = imfilter(double(img), top, 'replicate');
+bop = ones(19 , 19);
+r = floor(len/2);
+bop(10-r:10+r, 10-r:10+r) = 0;
+bop = bop/sum(bop(:));
+mb = imfilter(double(img), bop, 'replicate');
+D1 = (mt - mb ).^2.*(mt - mb >=0);
 %%
 op1 = zeros(len+2, len+2);
 op2 = op1;
 op3 = op1;
 op4 = op1; 
-op1(ly+2, lx+2:end-1) = 1;
-op1(ly+2, end) = -(lx+1);
-op2(ly+2, 2:lx+2) = 1;
-op2(ly+2, 1) = -(lx+1);
-op3(ly+2:end-1, lx+2) = 1;
-op3(end, lx+2) = -(ly+1);
-op4(2:ly+2, lx+2) = 1;
-op4(1, lx+2) = -(ly+1);
+op1(r+2, r+2:end-1) = 1;
+op1(r+2, end) = -(r+1);
+op2(r+2, 2:r+2) = 1;
+op2(r+2, 1) = -(r+1);
+op3(r+2:end-1, r+2) = 1;
+op3(end, r+2) = -(r+1);
+op4(2:r+2, r+2) = 1;
+op4(1, r+2) = -(r+1);
 
-g1 = imfilter(img, op1, 'symmetric');
+g1 = imfilter(img, op1, 'replicate');
 g1 = g1.*(g1>=0);
-g2 = imfilter(img, op2, 'symmetric');
+g2 = imfilter(img, op2, 'replicate');
 g2 = g2.*(g2>=0);
-g3 = imfilter(img, op3, 'symmetric');
+g3 = imfilter(img, op3, 'replicate');
 g3 = g3.*(g3>=0);
-g4= imfilter(img, op4, 'symmetric');
+g4= imfilter(img, op4, 'replicate');
 g4 = g4.*(g4>=0);
 temp =cat(3, g1, g2, g3, g4);
 D2 = min(temp, [], 3);
--- admdfunc.m
+++ admdfunc.m
@@ -1,47 +1,49 @@
 % Fast and robust small infrared target detection using absolute directional mean difference algorithm
-% 1 双框计算对比度（置零）
-% 2 多尺度
+% 1 多尺度： 目标框的尺度3x3 - 9x9，背景框的尺寸是目标框尺寸的3倍
+%     lcm是                 目标区域最大值^2
+%                                -----------------
+%                    周围8个背景区域平均值的最大值
+% admd是 （目标区域平均值 -  周围8个背景区域平均值的最大值）^2
 function re = admdfunc(img)
 img = double(img);
 %% the scale is 3
-out3 = subfubc(img, 3);
+out3 = subfunc(img, 3);
 %% the scale is 5
-out5 = subfubc(img, 5);
+out5 = subfunc(img, 5); 
 %% the scale is 7
-out7 = subfubc(img, 7);
+out7 = subfunc(img, 7); 
 %%  the scale is 9
-out9 = subfubc(img, 9);
+out9 = subfunc(img, 9); 
 %%
 out = cat(3, out3, out5, out7, out9);
 re = max(out, [], 3);
 if 0
+    figure; imshow(uint8(img));
     figure; imshow(out3, []);  title('scale 3x3');
-    figure; surf(out3); title('scale 3x3');
     figure; imshow(out5, []);   title('scale 5x5');
-    figure; surf(out5); title('scale 5x5');
     figure; imshow(out7, []);   title('scale 7x7');
-    figure; surf(out7); title('scale 7x7');
     figure; imshow(out9, []);   title('scale 9x9');
-    figure; surf(out9); title('scale 9x9');
     figure; imshow(re, []);
-    figure; surf(re);
+	 figure; surf(out3); title('scale 3x3');
+	   figure; surf(out5); title('scale 5x5');
+	  figure; surf(out7); title('scale 7x7');
+	  figure; surf(out9); title('scale 9x9');
 end
 end
-function out3 = subfubc(img, len)
-op35 = ones(len)/(len*len);
-m35 = imfilter(double(img), op35, 'symmetric');
-lx = floor(len/2);
-ly = floor(len/2);
+function re = subfunc(img, len)
+opt = ones(len)/(len*len);
+mt = imfilter(double(img), opt, 'replicate');
+r = floor(len/2);
 op = zeros(len * 3);
-op(ly+1, lx+1) = 1;
-op(ly+1, len + lx + 1) = 1;
-op(ly+1, len*2 + lx + 1) = 1;
-op(len + ly+1, lx+1) = 1;
-op(len + ly+1, len + lx + 1) = 0;
-op(len + ly+1, len*2 + lx + 1) = 1;
-op(len*2 + ly+1, lx+1) = 1;
-op(len*2 + ly+1, len + lx + 1) = 1;
-op(len*2 + ly+1, len*2 + lx + 1) = 1;
-m3b = imdilate(m35, op);
-out3 = (m35 - m3b).^2.*(m35 - m3b>0);
+op(r+1, r+1) = 1;
+op(r+1, len + r + 1) = 1;
+op(r+1, len*2 + r + 1) = 1;
+op(len + r+1, r+1) = 1;
+op(len + r+1, len + r + 1) = 0;
+op(len + r+1, len*2 + r + 1) = 1;
+op(len*2 + r+1, r+1) = 1;
+op(len*2 + r+1, len + r + 1) = 1;
+op(len*2 + r+1, len*2 + r + 1) = 1;
+mb = imdilate(mt, op);
+re = (mt - mb).^2.*(mt - mb>0);
 end
